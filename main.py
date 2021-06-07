98f3c6443b9a9e18c7a883b0bceec8f041d70c34
Syb Scholing <>
2021-06-07 10:00:04 +0000

    woordenlijst ingevoerd

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("Voer uw naam in alstublieft:")
 name = input()
 print("Hallo " + name + "!")
+
+counter = 0
+woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk" ]
+
+hetwoord = random.choice(woordenlijst)
+lengtewoord = len(hetwoord)
+temp = "." * lengtewoord
