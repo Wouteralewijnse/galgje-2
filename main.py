7c370357d8f24be981302970e8c9e13fb0c327c4
WouterAlewijnse <74767434+Wouteralewijnse@users.noreply.github.com>
2021-06-11 08:29:45 +0000

    Commentaar begin aangepast time sleeps toegevoegd

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ hetwoord = random.choice(woordenlijst)
 lengtewoord = len(hetwoord)
 temp = "." * lengtewoord
 
-print("We gaan lekker galgje spelen")
+print("We gaan vandaag galgje spelen")
+
+time.sleep(2)
+
 print("Ben je klaar om de strijd aan te gaan?")
 
 time.sleep(3)
 
 print()
 print("Je hebt 5 levens bij elke fout geraden letter gaat er een leven af")
+
+time.sleep(1)
+
+print('Zijn je 5 levens op zonder het woord te raden dan heb je verloren')
